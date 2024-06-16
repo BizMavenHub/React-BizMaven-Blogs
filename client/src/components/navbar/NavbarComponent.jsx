113eaafd735ef7e9bc1885b7537df903a9677d99
TepChoulong <tepchoulongzin@gmail.com>
2024-06-16 18:02:46 +0700

    make a slightly changes (justify-between)

--- client/src/components/navbar/NavbarComponent.jsx
+++ client/src/components/navbar/NavbarComponent.jsx
@@ -86,7 +86,7 @@ const NavbarComponent = () => {
     <>
       {currentUser ? (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
-          <div className="max-w-screen flex items-center justify-evenly mx-auto p-2">
+          <div className="max-w-screen flex items-center justify-between mx-auto p-2 px-12">
             <div>
               <Link
                 to="/"
@@ -102,98 +102,8 @@ const NavbarComponent = () => {
                 </span>
               </Link>
             </div>
-            <div className="flex items-center justify-center">
-              <form action="" onSubmit={handleSearch}>
-                <input
-                  type="text"
-                  placeholder="Search..."
-                  className="p-2 w-[300px] rounded outline-none"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                />
-              </form>
-            </div>
-            <div className="flex items-center md:order-2">
-              <div className="flex items-center md:order-2 md:space-x-0 rtl:space-x-reverse">
-                <button
-                  type="button"
-                  className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600 mr-8"
-                  id="user-menu-button"
-                  aria-expanded="false"
-                  data-dropdown-toggle="user-dropdown"
-                  data-dropdown-placement="bottom"
-                  onClick={handleShowNavLinks}
-                >
-                  <img
-                    className="w-14 h-10 rounded-full object-cover"
-                    src={
-                      currentUser.pictureProfile
-                        ? currentUser.pictureProfile
-                        : "/src/assets/default_profile_picture.png"
-                    }
-                    alt="user photo"
-                  />
-                </button>
 
-                {currentUser.isAdmin && (
-                  <button
-                    onClick={() => (window.location.href = "/create-post")}
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm h-[40px] w-[120px] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                  >
-                    Create Post
-                  </button>
-                )}
-
-                {/* <!-- Dropdown menu --> */}
-                {showNavLinks && (
-                  <div
-                    className="absolute z-10 top-12 right-0 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-                    id="user-dropdown"
-                  >
-                    <div className="px-4 py-3">
-                      <span className="block text-sm text-gray-900 dark:text-white">
-                        {currentUser.username}
-                      </span>
-                      <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
-                        {currentUser.email}
-                      </span>
-                    </div>
-                    <ul className="py-2" aria-labelledby="user-menu-button">
-                      {currentUser.isAdmin && (
-                        <li>
-                          <Link
-                            onClick={() => {
-                              window.location.href = "/dashboard?tab=overview";
-                            }}
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                          >
-                            Dashboard
-                          </Link>
-                        </li>
-                      )}
-                      <li>
-                        <Link
-                          onClick={() => {
-                            window.location.href = "/dashboard?tab=profile";
-                          }}
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                        >
-                          Profile
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          onClick={handleLogout}
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                        >
-                          Sign out
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                )}
-              </div>
-
+            <div className="flex items-center">
               <div
                 className=" items-center justify-end w-full md:flex md:w-auto md:order-1 mr-8"
                 id="navbar-cta"
@@ -230,8 +140,110 @@ const NavbarComponent = () => {
                       Contact
                     </Link>
                   </li>
+                  <li>
+                    <Link
+                      onClick={() => {
+                        window.location.href = "/feedback";
+                      }}
+                      className="block font-normal text-sm py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    >
+                      Feedback
+                    </Link>
+                  </li>
                 </ul>
               </div>
+            </div>
+
+            <div className="flex items-center">
+              <div className="flex items-center justify-center mx-4">
+                <form action="" onSubmit={handleSearch}>
+                  <input
+                    type="text"
+                    placeholder="Search..."
+                    className="p-2 w-[350px] rounded outline-none"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                  />
+                </form>
+              </div>
+
+              <button
+                type="button"
+                className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600 mx-4"
+                id="user-menu-button"
+                aria-expanded="false"
+                data-dropdown-toggle="user-dropdown"
+                data-dropdown-placement="bottom"
+                onClick={handleShowNavLinks}
+              >
+                <img
+                  className="w-10 h-10 rounded-full object-cover "
+                  src={
+                    currentUser.pictureProfile
+                      ? currentUser.pictureProfile
+                      : "/src/assets/default_profile_picture.png"
+                  }
+                  alt="user photo"
+                />
+              </button>
+
+              {currentUser.isAdmin && (
+                <button
+                  onClick={() => (window.location.href = "/create-post")}
+                  className="text-white ml-4   bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm h-[40px] w-[120px] dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                >
+                  Create Post
+                </button>
+              )}
+
+              {/* <!-- Dropdown menu --> */}
+              {showNavLinks && (
+                <div
+                  className="absolute z-10 top-12 right-10 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                  id="user-dropdown"
+                >
+                  <div className="px-4 py-3">
+                    <span className="block text-sm text-gray-900 dark:text-white">
+                      {currentUser.username}
+                    </span>
+                    <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                      {currentUser.email}
+                    </span>
+                  </div>
+                  <ul className="py-2" aria-labelledby="user-menu-button">
+                    {currentUser.isAdmin && (
+                      <li>
+                        <Link
+                          onClick={() => {
+                            window.location.href = "/dashboard?tab=overview";
+                          }}
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        >
+                          Dashboard
+                        </Link>
+                      </li>
+                    )}
+                    <li>
+                      <Link
+                        onClick={() => {
+                          window.location.href = "/dashboard?tab=profile";
+                        }}
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        Profile
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        onClick={handleLogout}
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        Sign out
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              )}
             </div>
           </div>
         </nav>
@@ -269,6 +281,16 @@ const NavbarComponent = () => {
                     className="block font-normal text-sm tracking-[2px] py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => {
+                      window.location.href = "/blogs";
+                    }}
+                    className="block font-normal text-sm tracking-[2px] py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    Blogs
                   </Link>
                 </li>
                 <li>
