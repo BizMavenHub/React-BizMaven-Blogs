7c902bb5cdd683a534eb3f988de575640fe7dc3b
TepChoulong <tepchoulongzin@gmail.com>
2024-06-18 09:54:10 +0700

    commit some style changes

--- client/src/pages/404/NotFound_page.jsx
+++ client/src/pages/404/NotFound_page.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 const NotFound_page = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-white py-48">
+    <div className="flex items-center justify-center min-h-screen bg-white py-12">
       <div className="flex flex-col">
         <div className="flex flex-col items-center">
-          <div className="text-indigo-500 font-bold text-[64pt]">404</div>
+          <div className="text-indigo-500 font-bold text-5xl">404</div>
 
-          <div className="font-bold text-10xl xl:text-7xl lg:text-6xl md:text-5xl mt-10">
+          <div className="font-bold text-10xl xl:text-4xl lg:text-6xl md:text-5xl mt-10">
             This page does not exist
           </div>
 
@@ -17,7 +17,7 @@ const NotFound_page = () => {
           </div>
         </div>
 
-        <div className="flex flex-col mt-28 mb-24">
+        <div className="flex flex-col mt-20 mb-8">
           <div className="text-gray-400 font-bold uppercase">Continue With</div>
 
           <div className="flex flex-col items-stretch mt-5">
