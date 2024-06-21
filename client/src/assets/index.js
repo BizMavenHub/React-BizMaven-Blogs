c17268a77e031e9ccde0799d41d822921be145fe
TepChoulong <tepchoulongzin@gmail.com>
2024-06-21 18:11:33 +0700

    add wave svg

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -4,6 +4,7 @@ import facebook_icon from "./imgs/facebook-icon.png";
 import woman from "./imgs/business-woman-working-on-laptop-in-her-office.jpg";
 import profile_pic from "./default_profile_picture.png";
 import insight_loop_logo from "./insight_Loop_Logo-only_white-Bg-transparent.png";
+import wave from "./wave.svg";
 
 export {
   google_icon,
@@ -12,4 +13,5 @@ export {
   woman,
   profile_pic,
   insight_loop_logo,
+  wave,
 };
