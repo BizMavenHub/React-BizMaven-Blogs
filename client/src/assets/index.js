dc0732f8e505a8397cbdc4086a2cf38017091df8
TepChoulong <tepchoulongzin@gmail.com>
2024-06-23 11:20:39 +0700

    add images

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -4,7 +4,14 @@ import facebook_icon from "./imgs/facebook-icon.png";
 import woman from "./imgs/business-woman-working-on-laptop-in-her-office.jpg";
 import profile_pic from "./default_profile_picture.png";
 import insight_loop_logo from "./insight_Loop_Logo-only_white-Bg-transparent.png";
-import wave from "./wave.svg";
+
+import girl_sitting_and_reading from "./Homepage-Pictures/Girl_sitting_and_reading.png";
+import Introduction from "./Homepage-Pictures/Introduction.png";
+import Picture_0 from "./Homepage-Pictures/Picture_0.png";
+import Picture_1 from "./Homepage-Pictures/Picture_1.png";
+import Picture_2 from "./Homepage-Pictures/Picture_2.png";
+import Picture_3 from "./Homepage-Pictures/Picture_3.png";
+import Wave from "./Homepage-Pictures/Wave.png";
 
 export {
   google_icon,
@@ -13,5 +20,11 @@ export {
   woman,
   profile_pic,
   insight_loop_logo,
-  wave,
+  girl_sitting_and_reading,
+  Introduction,
+  Picture_0,
+  Picture_1,
+  Picture_2,
+  Picture_3,
+  Wave,
 };
