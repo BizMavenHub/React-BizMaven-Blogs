02398e9e52e50c352b31bd645b958d21cd82aafb
TepChoulong <tepchoulongzin@gmail.com>
2024-07-04 17:40:41 +0700

    fix some width

--- client/src/containers/dashboard/DashCommentContainer.jsx
+++ client/src/containers/dashboard/DashCommentContainer.jsx
@@ -123,7 +123,7 @@ const DashCommentContainer = () => {
       <div className="relative overflow-x-auto sm:rounded-lg">
         {currentUser.isAdmin && comments.length > 0 ? (
           <>
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                   <th scope="col" className="px-6 py-3">
