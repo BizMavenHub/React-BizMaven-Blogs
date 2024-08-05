4313e98ae5cf2ea88407d1876ce592779636ce85
TepChoulong <tepchoulongzin@gmail.com>
2024-08-05 10:52:46 +0700

    remove unnecessary images

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,4 @@
 import google_icon from "./imgs/google-icon.png";
-import github_icon from "./imgs/github-icon.png";
-import facebook_icon from "./imgs/facebook-icon.png";
-import woman from "./imgs/business-woman-working-on-laptop-in-her-office.jpg";
-import profile_pic from "./default_profile_picture.png";
-import post_picture from "./imgs/post_picture.png";
 import insight_loop_logo from "./insight_Loop_Logo-only_white-Bg-transparent.png";
 
 import Girl_sitting_and_reading from "./Homepage-Pictures/Girl_sitting_and_reading.png";
@@ -25,12 +20,7 @@ import Picture_3_career from "./Career-Pictures/Picture_3.png";
 
 export {
   google_icon,
-  github_icon,
-  facebook_icon,
-  woman,
-  profile_pic,
   insight_loop_logo,
-  post_picture,
   Girl_sitting_and_reading,
   Introduction,
   Picture_0_homepage,
