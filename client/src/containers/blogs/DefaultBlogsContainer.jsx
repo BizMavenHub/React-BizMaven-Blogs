db10c9d24d5c4a6ed5d5a6cc55d999ce6a8674e3
TepChoulong <tepchoulongzin@gmail.com>
2024-08-09 20:57:59 +0700

    change loader and fix some layout

--- client/src/containers/blogs/DefaultBlogsContainer.jsx
+++ client/src/containers/blogs/DefaultBlogsContainer.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { BlogCard, RecentBlogCard } from "../../components";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { useCookies } from "react-cookie";
 
@@ -46,22 +46,22 @@ const DefaultBlogsContainer = () => {
       expires: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 30),
     });
     GetPosts();
-    GetPostsByLimit();
+  }, []);
+
+  const getPostsUrl = useMemo(() => {
+    return `${import.meta.env.VITE_API_BASE_URL}/api/post/get-post`;
   }, []);
 
   const GetPosts = async () => {
     setLoading(true);
     try {
-      const response = await fetch(
-        `${import.meta.env.VITE_API_BASE_URL}/api/post/get-post`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-        }
-      );
+      const response = await fetch(getPostsUrl, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
 
       const data = await response.json();
 
@@ -208,11 +208,11 @@ const DefaultBlogsContainer = () => {
 
   const DesktopView = () => {
     return (
-      <div className="blogs-container my-4">
-        <div className="card-container w-[600px] m-auto grid">
-          <div className="title my-8">
-            <h1 className="text-3xl font-medium">For you</h1>
-          </div>
+      <div className="blogs-container my-4 m-auto w-[1200px]">
+        <div className="title my-8">
+          <h1 className="text-3xl font-medium">For you</h1>
+        </div>
+        <div className="card-container grid grid-cols-3 gap-8">
           {allPosts.map((post, index) => (
             <BlogCard
               key={index}
@@ -234,22 +234,18 @@ const DefaultBlogsContainer = () => {
 
   const LargeDesktopView = () => {
     return (
-      <div className="blogs-container my-4">
-        <div className="card-container w-[600px] m-auto grid">
-          <div className="title my-8">
-            <h1 className="text-3xl font-medium">For you</h1>
-          </div>
+      <div className="blogs-container my-4 w-[80%]  m-auto">
+        <div className="title my-8">
+          <h1 className="text-3xl font-medium">For you</h1>
+        </div>
+        <div className="card-container grid grid-cols-4 gap-8">
           {allPosts.map((post, index) => (
-            <BlogCard
-              key={index}
-              id={post._id}
-              writerId={post.userId}
+            <RecentBlogCard
               title={post.title}
               image={post.image}
-              slug={post.slug}
               category={post.category}
-              keywords={post.keywords}
               content={post.content}
+              slug={post.slug}
               date={post.createdAt}
             />
           ))}
@@ -266,9 +262,27 @@ const DefaultBlogsContainer = () => {
       </Helmet>
       <>
         {loading ? (
-          <div className="loading-container h-min-screen h-full flex justify-center items-center">
-            <div className="loading">
-              <h3 className="text-3xl font-bold text-center">Loading...</h3>
+          <div className="loader-container h-screen flex justify-center items-center">
+            <div class="text-center">
+              <div role="status">
+                <svg
+                  aria-hidden="true"
+                  class="inline w-12 h-12 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="sr-only">Loading...</span>
+              </div>
             </div>
           </div>
         ) : (
