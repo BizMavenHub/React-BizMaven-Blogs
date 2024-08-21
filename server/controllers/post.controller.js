e56c0c7a544b932f3e61cc09fae721ea76b4f6d3
TepChoulong <tepchoulongzin@gmail.com>
2024-08-21 20:43:19 +0700

    update limited post

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -42,7 +42,7 @@ export const getPost = async (req, res, next) => {
 export const getPosts = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
-    const limit = parseInt(req.query.limit) || 9;
+    const limit = parseInt(req.query.limit) || 12;
     const sortDirection = req.query.order === "asc" ? 1 : -1;
 
     const posts = await Post.find({
