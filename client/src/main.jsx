8f7eac8b134d0b8dd25bd053a4e41d145cd6abdb
TepChoulong <tepchoulongzin@gmail.com>
2024-08-21 21:08:58 +0700

    add Vercel SpeedInsight

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./index.css";
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
+      <SpeedInsights />
     </PersistGate>
   </Provider>
 );
