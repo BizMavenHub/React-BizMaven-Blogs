76a454637ff4422fe94b8da72a5e9dbcef095a2a
TepChoulong <tepchoulongzin@gmail.com>
2024-09-09 11:59:26 +0700

    add footer component

--- client/src/pages/about_us/About_us_page.jsx
+++ client/src/pages/about_us/About_us_page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import { About_us_Container } from "../../containers/index";
+import { FooterComponent } from "../../components";
 
 function About_us_page() {
   return (
     <div>
       <About_us_Container />
+      <FooterComponent />
     </div>
   );
 }
--- client/src/pages/feedback/Feedback_page.jsx
+++ client/src/pages/feedback/Feedback_page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Feedback_Container } from "../../containers/index.js";
+import { FooterComponent } from "../../components";
 
 const Feedback_page = () => {
   return (
     <div>
       <Feedback_Container />
+      <FooterComponent />
     </div>
   );
 };
