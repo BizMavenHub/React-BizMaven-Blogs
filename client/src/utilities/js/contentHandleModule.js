0fc098de699e63b06e9720c7c54c4ad830752ddd
TepChoulong <tepchoulongzin@gmail.com>
2024-09-29 16:30:59 +0700

    create some utils

---
+++ client/src/utilities/js/contentHandleModule.js
@@ -0,0 +1,10 @@
+const headingModules =
+  " prose-headings:my-3 prose-headings:font-heading_font prose-headings:font-bold prose-h1:text-3xl prose-h2:text-2xl prose-h3:text-lg prose-p:text-base prose-p:mb-4 prose-p:font-paragraph_font prose-a:text-blue-500";
+
+const listModules =
+  "prose-a:no-underline prose-ol:list-decimal prose-ul:list-disc prose-ul:ml-10 prose-ul:mb-4 prose-ol:ml-10 prose-ol:mb-4 prose-li:font-paragraph_font";
+
+const blockQuoteModule =
+  "prose-blockquote:text-gray-700 prose-blockquote:font-paragraph_font prose-blockquote:mb-4 prose-blockquote:ml-4 prose-blockquote:border-l-4 prose-blockquote:pl-4";
+
+export { headingModules, listModules, blockQuoteModule };
---
+++ client/src/utilities/js/handleReloadPage.js
@@ -0,0 +1,6 @@
+// Functions to handle navigation to different pages and reload the page
+function handleReloadPage(path) {
+  return (window.location.href = path);
+}
+
+export { handleReloadPage };
