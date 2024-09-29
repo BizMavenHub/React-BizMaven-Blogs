1e46ff59632d9c17b025e3b1efabf18908b6881a
TepChoulong <tepchoulongzin@gmail.com>
2024-09-29 17:00:30 +0700

    add new screen

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -19,7 +19,7 @@ export default {
       xl: "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      "2xl": "1536px",
+      xxl: "1536px",
       // => @media (min-width: 1536px) { ... }
     },
     fontFamily: {
