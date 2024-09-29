67c10d6219a234ab51da967844871f6c857c7387
TepChoulong <tepchoulongzin@gmail.com>
2024-09-29 16:31:17 +0700

    add a route for get most viewed posts

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -2,22 +2,20 @@ import express from "express";
 import verifyUser from "../utils/verifyUser.js";
 
 import {
-  createPost,
-  getPost,
   getPosts,
-  deletePost,
+  getMostViewedPosts,
+  createPost,
   updatePost,
-  getAllPostsFromDB,
+  deletePost,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.post("/create-post", verifyUser, createPost);
-router.get("/get-post/:postId", getPost);
 router.get("/get-post", getPosts);
-router.delete("/delete-post/:postId", verifyUser, deletePost);
-router.put("/update-post/:postId/:userId", verifyUser, updatePost);
+router.get("/get-most-viewed-posts", getMostViewedPosts);
 
-router.get("/get-all-posts-from-db", getAllPostsFromDB);
+router.post("/create-post", verifyUser, createPost);
+router.put("/update-post/:postId/:userId", verifyUser, updatePost);
+router.delete("/delete-post/:postId", verifyUser, deletePost);
 
 export default router;
