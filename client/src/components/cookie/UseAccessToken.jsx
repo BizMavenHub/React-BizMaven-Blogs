2a7148b69f2c1b9a59f5c24d91b464fb3fbe8d0b
TepChoulong <tepchoulongzin@gmail.com>
2024-10-12 14:20:14 +0700

    remove domain

--- client/src/components/cookie/UseAccessToken.jsx
+++ client/src/components/cookie/UseAccessToken.jsx
@@ -32,7 +32,6 @@ const UseAccessToken = () => {
         expires: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 30), // 1 month
         secure: true,
         sameSite: "none",
-        domain: "https://www.insightloop.blog",
       });
       setHasAccessToken(true);
     }
