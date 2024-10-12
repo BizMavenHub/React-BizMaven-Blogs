d2b7a9511140a3341888a0e4334f6e388d4e279c
TepChoulong <tepchoulongzin@gmail.com>
2024-10-12 14:28:13 +0700

    testing cookie

--- server/controllers/auth/auth.controller.js
+++ server/controllers/auth/auth.controller.js
@@ -68,10 +68,12 @@ export async function loginWithEmail(req, res, next) {
 
     res
       .status(200)
+
       .cookie("access_token", token, {
         expires: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 30),
         secure: true,
         sameSite: "none",
+        httpOnly: true,
       })
       .json({ message: "Login successfully", ...rest });
   } catch (error) {
@@ -101,6 +103,7 @@ export async function loginWithGoogle(req, res, next) {
           expires: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 30), // 30 days
           secure: true,
           sameSite: "none",
+          httpOnly: true,
         })
         .json({ message: "Login successfully", ...rest });
     } else {
@@ -133,6 +136,7 @@ export async function loginWithGoogle(req, res, next) {
           expires: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 30), // 30 days
           secure: true,
           sameSite: "none",
+          httpOnly: true,
         })
         .json({ message: "Login successfully", ...rest });
     }
